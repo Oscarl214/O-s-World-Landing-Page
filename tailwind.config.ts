479a8aa4fa6fb33422fe5c05d2a89d05b2f0c62e
Oscar Leal <oscarleal234@gmail.com>
2024-06-24 20:50:31 -0500

    setting up providers

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+const { nextui } = require('@nextui-org/react');
+const { addDynamicIconSelectors } = require('@iconify/tailwind');
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    addDynamicIconSelectors(),
+    nextui({
+      themes: {
+        light: {
+          layout: {
+            foreground: '#000000',
+          }, // light theme layout tokens
+        },
+        dark: {
+          layout: {}, // dark theme layout tokens
+          colors: {
+            foreground: '#ffffff',
+          }, // dark theme colors
+        },
+      },
+    }),
+  ],
 };
 export default config;
